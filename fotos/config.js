330f8ca58e634f71ad25521a733cfb30a730bfb8
Steve Roberts <mbudm.com@gmail.com>
2018-02-24 13:53:56 +1100

    syncing the output which is accessible via a config lambda
    this will be used by the client in a sep project to kick off auth.

---
+++ fotos/config.js
@@ -0,0 +1,25 @@
+
+import s3 from './lib/s3';
+import { success, failure } from './lib/responses';
+
+export function getS3Params() {
+  const Bucket = process.env.S3_OUTPUT_BUCKET;
+  const Key = process.env.S3_OUTPUT_FILENAME;
+  console.log('getS3Params', Bucket, Key);
+  return {
+    Bucket,
+    Key,
+  };
+}
+
+export async function getItem(event, context, callback) {
+  const s3Params = getS3Params();
+  try {
+    const s3Object = await s3.getObject(s3Params).promise();
+    const parsedBuffer = JSON.parse(s3Object.Body.toString());
+    return callback(null, success(parsedBuffer));
+  } catch (err) {
+    console.error(err, s3Params);
+    return callback(null, failure(err));
+  }
+}
---
+++ output/README.md
@@ -0,0 +1,2 @@
+Store config for client access that is available via an end point
+This folder gets synced to S3
