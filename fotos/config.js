1c627ba59f9d104458757a930d85b2a5bacd62c1
Steve Roberts <mbudm.com@gmail.com>
2018-03-08 19:19:32 +1100

    Let offline just use the last deployed output

--- fotos/config.js
+++ fotos/config.js
@@ -2,13 +2,6 @@
 import s3 from './lib/s3';
 import { success, failure } from './lib/responses';
 
-const offlineConfig = {
-  UserPoolId: 'us-east-1_FaKe12345',
-  UserPoolClientId: 'FakeUserPoolClientId',
-  IdentityPoolId: 'FakeIdentityPoolId',
-  Region: 'us-east-1',
-};
-
 export function getS3Params() {
   const Bucket = process.env.S3_OUTPUT_BUCKET;
   const Key = process.env.S3_OUTPUT_FILENAME;
@@ -20,9 +13,6 @@ export function getS3Params() {
 }
 
 export async function getItem(event, context, callback) {
-  if (process.env.IS_OFFLINE) {
-    return callback(null, success(offlineConfig));
-  }
   const s3Params = getS3Params();
   try {
     const s3Object = await s3.getObject(s3Params).promise();
