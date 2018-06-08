10a74fa8fb2fc9e04e4afc283cef638e6d6a2136
Steve Roberts <steve.roberts@xero.com>
2018-06-08 21:21:42 +1000

    thumbs creator WIP

---
+++ fotos/thumbs.js
@@ -0,0 +1,73 @@
+
+import AWS from 'aws-sdk';
+import Sharp from 'sharp';
+
+const S3 = new AWS.S3({
+  signatureVersion: 'v4',
+});
+
+const {
+  S3_BUCKET,
+} = process.env;
+const SIZES = [
+  [80, 80],
+  [160, 200],
+  [600, 400],
+].map(sizes => ({
+  width: sizes[0],
+  height: sizes[1],
+}));
+
+export function getObject(key) {
+  return S3.getObject({ Bucket: S3_BUCKET, Key: key }).promise();
+}
+
+export function putObject({
+  buffer, key, width, height,
+}) {
+  return S3.putObject({
+    Body: buffer,
+    Bucket: S3_BUCKET,
+    ContentType: 'image/png',
+    Key: `${width}x${height}/${key}`,
+  }).promise();
+}
+
+export function resize({ data, width, height }) {
+  return Sharp(data.Body)
+    .resize(width, height)
+    .background({
+      r: 255, g: 255, b: 255, alpha: 0,
+    })
+    .embed()
+    .toFormat('png')
+    .toBuffer();
+}
+
+export function resizeAndUpload({
+  data, width, height, key,
+}) {
+  return resize({ data, width, height })
+    .then(buffer => putObject({
+      buffer, key, width, height,
+    }));
+}
+
+export function resizeAndUploadSizes({ data, key }) {
+  return Promise.all(SIZES.map(({ width, height }) => resizeAndUpload({
+    width, height, data, key,
+  })));
+}
+
+export async function createThumb(event) {
+  const record = event.Records[0].s3;
+  const { key } = record.object;
+  console.log('thumbs', record, key);
+  try {
+    const data = await getObject(key);
+    const result = await resizeAndUploadSizes({ data, key });
+    console.log(result);
+  } catch (e) {
+    console.error('thumbs error:', e);
+  }
+}
