a975eb1f759d90bc590537de8b32812370afae53
Steve Roberts <mbudm.com@gmail.com>
2019-08-02 14:49:23 +1000

    query result limit and date limit (issues #2 and #3)

--- fotos/types/query.ts
+++ fotos/types/query.ts
@@ -1,4 +1,6 @@
+import { ItemList } from "aws-sdk/clients/dynamodb";
 import { ITraceMeta } from "./common";
+import { IImageMeta } from "./images";
 
 export interface IQueryBody {
   traceMeta?: ITraceMeta;
@@ -6,7 +8,28 @@ export interface IQueryBody {
     tags: string[],
     people: string[],
   };
+  lastRetrievedBirthtime?: number;
+  limit?: number;
   from: number;
   to: number;
   username: string;
+}
+
+export interface IQueryDBResponseItem {
+  birthtime: string;
+  group: string;
+  meta: IImageMeta;
+  id: string;
+  img_key: string;
+  img_thumb_key: string;
+  people?: string[];
+  tags?: string[];
+  userIdentityId: string;
+  username: string;
+}
+
+export interface IQueryResponse {
+  items: IQueryDBResponseItem[];
+  message: string;
+  remainingResults: number;
 }
