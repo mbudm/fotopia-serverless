1f77932a91dc1836ca0ffa325b5e783deda30e9d
Steve Roberts <mbudm.com@gmail.com>
2019-08-04 10:07:54 +1000

    Revert "Revert "clean up after functional test""
    This reverts commit 77aea7362705fedaa352a2568d0df200f535b33d.

--- test/functional.js
+++ test/functional.js
@@ -181,8 +181,6 @@ export default function (auth, api, upload) {
   });
 
   test('query all', (t) => {
-    t.plan(2);
-
     const query = {
       username,
       criteria: {
@@ -199,6 +197,7 @@ export default function (auth, api, upload) {
       .then((responseBody) => {
         t.ok(responseBody.items.find(rec => rec.id === records[0].id), 'find record 0');
         t.ok(responseBody.items.find(rec => rec.id === records[1].id), 'find record 1');
+        t.end();
       })
       .catch(formatError);
   });
@@ -414,7 +413,7 @@ export default function (auth, api, upload) {
   });
 
   test('peopleMerge', (t) => {
-    if (people.length > 0) {
+    if (people.length > 1) {
       const body = people
         .reduce((accum, person) => (accum.length < 2 ? accum.concat(person.id) : accum), []);
       api.post(apiUrl, '/people/merge', {
@@ -431,7 +430,7 @@ export default function (auth, api, upload) {
   });
 
   test('getPeople - check peopleMerge', (t) => {
-    if (people.length > 0) {
+    if (people.length > 1) {
       api.get(apiUrl, '/people')
         .then((responseBody) => {
           t.equal(responseBody.length, people.length - 1, 'one less person');
@@ -441,6 +440,34 @@ export default function (auth, api, upload) {
     } else {
       t.end();
     }
+  });
+
+  test('delete all records', (t) => {
+    const query = {
+      username,
+      criteria: {
+        people: [],
+        tags: [],
+      },
+      from: '2000-01-1',
+      to: Date.now(),
+      breakDateRestriction: true,
+      clientId: 'functionalTestCleanup',
+    };
+
+    api.post(apiUrl, '/query', {
+      body: query,
+    })
+      .then(responseBody => Promise.all(responseBody.items.map((item) => {
+        const apiPath = getEndpointPath({ ...item, username });
+        console.log(`Deleting ${apiPath}`);
+        return api.del(apiUrl, apiPath);
+      })))
+      .then((deleteResponses) => {
+        t.equal(deleteResponses, []);
+        t.end();
+      })
+      .catch(formatError);
   });
 
   test('force kill amplify process', (t) => {
