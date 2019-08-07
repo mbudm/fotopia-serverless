35923598dea6d397437aea1f33d76700f7de764c
Steve Roberts <mbudm.com@gmail.com>
2019-08-07 11:18:33 +1000

    add rekognition create/delete lambdas

---
+++ fotos/collection.ts
@@ -0,0 +1,89 @@
+import { Rekognition } from "aws-sdk";
+import {
+  CreateCollectionResponse,
+  DeleteCollectionResponse,
+} from "aws-sdk/clients/rekognition";
+import * as uuid from "uuid";
+import { failure, success } from "./common/responses";
+import logger from "./lib/logger";
+import { ILoggerBaseParams } from "./types";
+
+export function getCreateLogParams(
+  name: string,
+  params: CreateCollectionResponse,
+) {
+  return {
+    rekognitionCollectionArn: params.CollectionArn,
+    rekognitionCollectionName: name,
+    rekognitionFaceModelVersion: params.FaceModelVersion,
+    rekognitionStatusCode: params.StatusCode,
+  };
+}
+
+export async function createCollection(event, context, callback) {
+  const startTime = Date.now();
+  const loggerBaseParams: ILoggerBaseParams = {
+    id: uuid.v1(),
+    name: "createCollection",
+    parentId: "",
+    startTime,
+    traceId: uuid.v1(),
+  };
+  try {
+    const rekognitionClient = new Rekognition();
+    const params = {
+      CollectionId: process.env.REKOGNITION_COLLECTION as string,
+    };
+    const rekResponse = await rekognitionClient
+      .createCollection(params)
+      .promise();
+    logger(
+      context,
+      loggerBaseParams,
+      getCreateLogParams(params.CollectionId, rekResponse),
+    );
+    return callback(null, success(rekResponse));
+  } catch (err) {
+    logger(context, loggerBaseParams, { err });
+    return callback(null, failure(err));
+  }
+}
+
+export function getDeleteLogParams(
+  name: string,
+  params: DeleteCollectionResponse,
+) {
+  return {
+    rekognitionCollectionName: name,
+    rekognitionStatusCode: params.StatusCode,
+  };
+}
+
+export async function deleteCollection(event, context, callback) {
+  const startTime = Date.now();
+  const loggerBaseParams: ILoggerBaseParams = {
+    id: uuid.v1(),
+    name: "deleteCollection",
+    parentId: "",
+    startTime,
+    traceId: uuid.v1(),
+  };
+  try {
+    const rekognitionClient = new Rekognition();
+    const params = {
+      CollectionId: process.env.REKOGNITION_COLLECTION as string,
+    };
+    const rekResponse = await rekognitionClient
+      .deleteCollection(params)
+      .promise();
+    logger(
+      context,
+      loggerBaseParams,
+      getDeleteLogParams(params.CollectionId, rekResponse),
+    );
+    return callback(null, success(rekResponse));
+  } catch (err) {
+    logger(context, loggerBaseParams, { err });
+    return callback(null, failure(err));
+  }
+}
