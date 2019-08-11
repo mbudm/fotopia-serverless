6186ffb3c392b860898b13d5dd5e06b21fe8f295
Steve Roberts <mbudm.com@gmail.com>
2019-08-11 19:27:44 +1000

    fix functional tests and redeploy to reset env

--- test/functional/people.ts
+++ test/functional/people.ts
@@ -70,8 +70,6 @@ export default function peopleTests(setupData, api) {
   let imageWithPerson1: IImage;
 
   test("get (query) image with person 1 before we merge", (t) => {
-    t.plan(3);
-
     const query: IQueryBody = {
       criteria: {
         people: [people[1].id],
@@ -85,14 +83,15 @@ export default function peopleTests(setupData, api) {
       .post(setupData.apiUrl, "/query", {
         body: query,
       })
-      .then((responseBody) => {
+      .then((responseBody: IImage[]) => {
         t.equal(responseBody.length, 1);
         t.equal(
-          responseBody[0].people.contains(people[1].id),
+          responseBody[0].people!.includes(people[1].id),
           true,
           "image has person 1",
         );
         imageWithPerson1 = responseBody[0];
+        t.end();
       })
       .catch(formatError);
   });
@@ -115,16 +114,15 @@ export default function peopleTests(setupData, api) {
       .get(setupData.apiUrl, "/people")
       .then((responseBody: IPerson[]) => {
         t.equal(responseBody.length, people.length - 1, "one less person");
-        t.equal(responseBody[0].id, [people[0].id], "person 0 is the same");
-        t.equal(responseBody[1].id, [people[2].id], "person 2 is now at pos 1");
-        t.equal(responseBody[2].id, [people[3].id], "person 3 is now at pos 2");
+        t.equal(responseBody[0].id, people[0].id, "person 0 is the same");
+        t.equal(responseBody[1].id, people[2].id, "person 2 is now at pos 1");
+        t.equal(responseBody[2].id, people[3].id, "person 3 is now at pos 2");
         t.end();
       })
       .catch(formatError);
   });
 
   test("after merge image that had person 1 now has person 0", (t) => {
-    t.plan(2);
     const apiPath = getEndpointPath(imageWithPerson1);
     api
       .get(setupData.apiUrl, apiPath)
@@ -145,6 +143,7 @@ export default function peopleTests(setupData, api) {
           true,
           "image has person 0",
         );
+        t.end();
       })
       .catch(formatError);
   });
