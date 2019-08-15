6f4e9b687a7c2470a3ecc587b798e162cce94586
Steve Roberts <mbudm.com@gmail.com>
2019-08-15 11:21:46 +1000

    remove unneeded upload.
    Fixing storage creds issue in setup now

--- test/functional/upload.ts
+++ test/functional/upload.ts
@@ -4,17 +4,6 @@ import { ISetupData } from "../types";
 import formatError from "./formatError";
 
 export default function uploadTests(setupData: ISetupData, upload: any) {
-  test("upload image two - don't create as this just forces storage to get creds... idk why", (t) => {
-    t.plan(1);
-    const object = fs.createReadStream(setupData.images[2].path);
-    upload(setupData.images[2].key, object, {
-      contentType: "image/jpeg",
-    })
-      .then((responseBody: any) => {
-        t.equal(responseBody.key, setupData.images[2].key);
-      })
-      .catch(formatError);
-  });
 
   test("upload image one", (t) => {
     t.plan(1);
