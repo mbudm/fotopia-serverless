b23cdc333f2b1a121f2362a88258b9e5565ee5d6
Steve Roberts <mbudm.com@gmail.com>
2019-08-19 11:06:28 +1000

    put people is an event invocation so their is no response containing the people array.

--- fotos/common/invokePutPeople.ts
+++ fotos/common/invokePutPeople.ts
@@ -1,15 +1,13 @@
 import {
   InvocationRequest,
-  InvocationResponse,
 } from "aws-sdk/clients/lambda";
-import { JSONParseError } from "../errors/jsonParse";
 import { INVOCATION_EVENT } from "../lib/constants";
 import lambda from "../lib/lambda";
 import {
   IPerson,
 } from "../types";
 
-export function getInvokeUpdateParams(body, traceMeta): InvocationRequest {
+export function getInvokeUpdateParams(body: IPerson[], traceMeta): InvocationRequest {
   return {
     FunctionName: process.env.IS_OFFLINE ? "peopleUpdate" : `${process.env.LAMBDA_PREFIX}peopleUpdate`,
     InvocationType: INVOCATION_EVENT,
@@ -25,18 +23,5 @@ export function getInvokeUpdateParams(body, traceMeta): InvocationRequest {
 
 export default function invokePutPeople(body, traceMeta) {
   const params = getInvokeUpdateParams(body, traceMeta);
-  return lambda.invoke(params).promise()
-    .then((invocationResponse: InvocationResponse) => {
-      try {
-        const payload = JSON.parse(invocationResponse.Payload as string);
-        const peopleObject: IPerson[] = JSON.parse(payload.body);
-        return peopleObject;
-      } catch (e) {
-        // tslint:disable-next-line:max-line-length
-        throw new JSONParseError(e, `invokeGetPeople, invocationResponse is : ${JSON.stringify(invocationResponse)}`);
-      }
-    })
-    .catch((e) => {
-      throw new JSONParseError(e, "invokeGetImageRecord");
-    });
+  return lambda.invoke(params).promise();
 }
