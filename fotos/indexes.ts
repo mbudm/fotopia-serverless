5f9d9d71f38c73825c0baacc55867b49429818e0
Steve Roberts <mbudm.com@gmail.com>
2019-08-28 19:16:48 +1000

    too noisy, just try the raw context by itself

--- fotos/indexes.ts
+++ fotos/indexes.ts
@@ -79,7 +79,7 @@ export async function getItem(event: APIGatewayProxyEvent, context: Context, cal
     JSON.parse(context.clientContext.Custom) :
     null ;
   // tslint:disable-next-line:no-console
-  console.log("client context", context, event);
+  console.log("client context", context);
   const loggerBaseParams: ILoggerBaseParams = {
     id: uuid.v1(),
     name: "getItem",
