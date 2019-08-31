d62880f704ffde614a0e93ed853baf90f1b3b0c0
Steve Roberts <mbudm.com@gmail.com>
2019-08-31 20:38:52 +1000

    get tests wait until people are added so that we know the event chain has completed
    However the indexes still fail occasionally as there is a chance that the S3 read/write is out of sync. TL:DR; need to use a queue

--- test/functional/get.ts
+++ test/functional/get.ts
@@ -5,8 +5,11 @@ import formatError from "./formatError";
 import getEndpointPath from "./getEndpointPath";
 
 export default function getTests(setupData: ISetupData, api: any) {
-  let imageWithFourPeople: IImage | undefined;
-  test("query all to get an id", (t) => {
+
+  const retryStrategy = [300, 500, 1000, 2000, 5000];
+  let imagesWithFourPeople: IImage[];
+  let imagesWithOnePerson: IImage[];
+  test("query all to get the test image ids", (t) => {
     t.plan(2);
 
     const query: IQueryBody = {
@@ -23,21 +26,60 @@ export default function getTests(setupData: ISetupData, api: any) {
       body: query,
     })
       .then((responseBody: IImage[]) => {
-        t.ok(responseBody.find((rec) => rec.img_key === setupData.records[0].img_key), "image one found");
-        imageWithFourPeople = responseBody.find((rec) => rec.img_key === setupData.records[1].img_key);
-        t.ok(imageWithFourPeople, "image with four people found");
+        imagesWithOnePerson = responseBody.filter((rec) => rec.img_key === setupData.records[0].img_key);
+        t.ok(imagesWithOnePerson.length > 0, "image(s) with one  personfound");
+        imagesWithFourPeople = responseBody.filter((rec) => rec.img_key === setupData.records[1].img_key);
+        t.ok(imagesWithFourPeople.length > 0, "image(s) with four people found");
       })
       .catch(formatError);
   });
 
-  test("get an item", (t) => {
-    t.plan(2);
-    const apiPath = getEndpointPath(imageWithFourPeople);
-    api.get(setupData.apiUrl, apiPath)
-      .then((responseBody: IImage) => {
-        t.equal(responseBody.img_key, imageWithFourPeople!.img_key, "response has same img_key");
-        t.equal(responseBody.id, imageWithFourPeople!.id, "response has same id");
-      })
-      .catch(formatError);
+  test("get items, retry until the record has the correct people count - shows the event chain is completed", (t) => {
+    const testImages = [...imagesWithOnePerson, ...imagesWithFourPeople];
+
+    testImages.forEach((image, idx, arr) => {
+      let retryCount = 0;
+      const retryableTest = {
+        args: [setupData.apiUrl, getEndpointPath(image)],
+        fn: api.get,
+      };
+      const retryableTestThen = (responseBody: IImage) => {
+        if (!responseBody.people || responseBody.people.length === 0) {
+          if (retryCount < retryStrategy.length) {
+            setTimeout(() => {
+              retryCount++;
+              t.comment(`Retry # ${retryCount} after ${retryStrategy[retryCount - 1]}ms`);
+              retry();
+            }, retryStrategy[retryCount]);
+          } else {
+            t.fail(`Failed - ${responseBody.img_key} people: ${responseBody.people} after ${retryCount} retries`);
+            if (idx === arr.length - 1) {
+              t.end();
+            }
+          }
+        } else {
+          const expectedLength = responseBody.img_key === imagesWithFourPeople[0].img_key ? 4 : 1;
+          t.equal(
+            responseBody.people.length,
+            expectedLength,
+            `image (${
+              responseBody.img_key
+            }) has people length of ${
+              responseBody.people.length
+            }`,
+          );
+          if (idx === arr.length - 1) {
+            t.end();
+          }
+        }
+      };
+      const retry = () => {
+        retryableTest.fn.apply(this, retryableTest.args)
+          .then(retryableTestThen)
+          .catch(formatError);
+      };
+
+      retry();
+    });
   });
 }
