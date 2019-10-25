4d74e1f808809fdd3dab2a0f4289b31bfb90f5e6
Steve Roberts <steve@commoncode.io>
2019-10-25 16:40:02 +1100

    fix validation of path param

--- indexes/src/common/validatePathParameters.ts
+++ indexes/src/common/validatePathParameters.ts
@@ -1,7 +1,7 @@
 import { IPathParameters } from "../types";
 
 const validatePathParameters = (params: any): IPathParameters => {
-  if (params && params.username && params.id) {
+  if (params && params.id) {
     return params as IPathParameters;
   } else {
     throw new Error("Invalid path parameters");
