7029d5087148e867a52e850f74e380984b15b502
Steve Roberts <mbudm.com@gmail.com>
2019-10-27 19:35:08 +1100

    remove indexes stack
    may split out like this as part of the overall breakup

--- indexes/src/common/getTableName.ts
+++
@@ -1,8 +0,0 @@
-const getTableName = (): string => {
-  if (process.env.DYNAMODB_TABLE) {
-    return process.env.DYNAMODB_TABLE;
-  } else {
-    throw new Error("No DYNAMODB_TABLE env variable set");
-  }
-};
-export default getTableName;
--- indexes/src/lib/dynamodb.ts
+++
@@ -1,7 +0,0 @@
-import * as AWS from "aws-sdk";
-
-const options = {};
-
-const client = new AWS.DynamoDB.DocumentClient(options);
-
-export default client;
