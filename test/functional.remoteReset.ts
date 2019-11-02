7139d06c132e5c04e1e6a0be6409737972b0fcd5
Steve Roberts <mbudm.com@gmail.com>
2019-11-02 15:00:50 +1100

    fix reset script

--- test/functional.remoteReset.ts
+++ test/functional.remoteReset.ts
@@ -3,11 +3,12 @@ import { config } from "dotenv";
 import deleteAllNotJustTestData from "./functional/deleteAll";
 import setup from "./functional/setup";
 import * as api from "./remote/api";
+import uploader from "./remote/upload";
 import auth from "./remote/auth";
 
 config();
 
-setup(auth)
+setup(auth, uploader, api)
   .then((setupData: any) => {
     deleteAllNotJustTestData(setupData, api);
   });
