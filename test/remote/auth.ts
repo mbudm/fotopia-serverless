a584da57249c774af0ecf3b0aa4b14b01c7ca562
Steve Roberts <steve@commoncode.io>
2020-06-16 21:46:45 +1000

    Revive new user auth

--- test/remote/auth.ts
+++ test/remote/auth.ts
@@ -2,13 +2,13 @@ import "isomorphic-fetch";
 
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 import * as AWS from "aws-sdk";
-import { CognitoIdentityCredentials } from "aws-sdk";
+import { CognitoIdentityCredentials, CognitoIdentityServiceProvider } from "aws-sdk";
 
-function authenticateExistingUser(config: any) {
+function authenticateUser(config: any, username?: string, password?: string) {
   return new Promise((resolve, reject) => {
     const authenticationData = {
-      Password : process.env.TEST_USER_PWD || "",
-      Username : process.env.TEST_USER_NAME || "",
+      Password : password || "",
+      Username : username || "",
     };
     const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
     const poolData = {
@@ -23,20 +23,77 @@ function authenticateExistingUser(config: any) {
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
     cognitoUser.authenticateUser(authenticationDetails, {
       onSuccess(result) {
-        const accessToken = result.getAccessToken().getJwtToken();
-
-        /* Use the idToken for Logins Map when Federating User Pools
-        with identity pools or when passing through an Authorization
-        Header to an API Gateway Authorizer*/
-        const idToken = result.getIdToken().getJwtToken();
-        resolve({
-          accessToken,
-          idToken,
-        });
+        resolve(result);
       },
       onFailure(err) {
         reject(err);
       },
+    });
+  });
+}
+
+function authenticateExistingUser(config: any) {
+  return authenticateUser(config, process.env.TEST_USER_NAME, process.env.TEST_USER_PWD)
+    .then((result: any) => {
+      const accessToken = result.getAccessToken().getJwtToken();
+      /* Use the idToken for Logins Map when Federating User Pools
+      with identity pools or when passing through an Authorization
+      Header to an API Gateway Authorizer*/
+      const idToken = result.getIdToken().getJwtToken();
+      return {
+        accessToken,
+        idToken,
+      };
+    });
+}
+
+function authenticateNewUser(config: any) {
+  return new Promise((resolve, reject) => {
+    const cognitoISP = new CognitoIdentityServiceProvider({
+      region: config.Region,
+    });
+    const username: string = process.env.TEST_USER_NAME || "";
+    const tempPassword = "@This1sChanged";
+    const params = {
+      DesiredDeliveryMediums: [
+        "EMAIL",
+      ],
+      ForceAliasCreation: true,
+      MessageAction: "SUPPRESS",
+      TemporaryPassword: tempPassword,
+      UserAttributes: [
+        {
+          Name: "email",
+          Value: process.env.TEST_USER_EMAIL,
+        },
+      ],
+      UserPoolId: config.UserPoolId,
+      Username: username,
+    };
+    cognitoISP.adminCreateUser(params, (err, d) => {
+      if (err) {
+        reject(err);
+      } else {
+        const setPwParams = {
+          Password:  process.env.TEST_USER_PWD || "",
+          Permanent: true,
+          UserPoolId: config.UserPoolId,
+          Username: username,
+        };
+        return new Promise((setPwResolve, setPwReject) => {
+            cognitoISP.adminSetUserPassword(setPwParams, (e, data) => {
+              if (e) {
+                setPwReject(e);
+              } else {
+                setPwResolve(data);
+              }
+            });
+          })
+          .then((response: any) => {
+            return resolve(authenticateUser(config, username, setPwParams.Password));
+          })
+          .catch(reject);
+      }
     });
   });
 }
@@ -91,8 +148,41 @@ function getAwsCredentials(config: any, userToken) {
   });
 }
 
+export function checkUserExists(config: any) {
+  const username = process.env.TEST_USER_NAME;
+  const cognitoISP = new CognitoIdentityServiceProvider({
+    region: config.Region,
+  });
+  return cognitoISP.listUsers({
+    Filter: `username = "${username}"`,
+    UserPoolId: config.UserPoolId,
+  }).promise()
+  .catch((e) => {
+    // tslint:disable-next-line:no-console
+    console.log(e);
+    return e;
+  });
+}
+function getAWSCredentialsFromProfile(){
+  return new Promise((resolve, reject) => {
+    AWS.config.getCredentials((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(AWS.config.credentials);
+      }
+    });
+  });
+}
+
 export default function auth(config: any) {
-  return authenticateExistingUser(config)
+  return getAWSCredentialsFromProfile()
+    .then(() => checkUserExists(config))
+    .then((response) => {
+      return response!.Users!.length === 1 ?
+        authenticateExistingUser(config) :
+        authenticateNewUser(config);
+    })
     .then(() => getCurrentUser(config))
     .then(getUserToken)
     .then((userToken) => getAwsCredentials(config, userToken))
